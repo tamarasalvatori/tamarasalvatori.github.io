c0bd570f9c26cd542e2a7ecba1246c103415e631
Tamara Salvatori <tamarasalvatori@gmail.com>
2021-05-08 15:46:48 -0300

    Adding right curriculo lattes urls

--- js/app.js
+++ js/app.js
@@ -36,7 +36,6 @@ ajax.onreadystatechange = function(){
         var data = ajax.responseText;
         var data_jason = JSON.parse(data);
         var content = document.getElementById("content");
-        var modal =  document.getElementById("modal")
 
         if(data_jason.length ==  0){
             content.innerHTML = '<div class="col-12"><div class="alert alert-warning" role="alert">Ainda não há palestras cadastradas</div></div>';
@@ -47,7 +46,7 @@ ajax.onreadystatechange = function(){
                 html_content += '<div class="row"><h2>'+data_jason[i].categoria+'</h2></div>'
 
                 for(var j=0; j<data_jason[i].itens.length; j++){
-                    html_content += card_item(data_jason[i].itens[j].nome, data_jason[i].itens[j].imagem, data_jason[i].itens[j].descricao, data_jason[i].itens[j].dia, data_jason[i].itens[j].hora, data_jason[i].itens[j].sala)
+                    html_content += card_item(data_jason[i].itens[j].nome, data_jason[i].itens[j].imagem, data_jason[i].itens[j].descricao, data_jason[i].itens[j].link)
                 }
             } 
             content.innerHTML = html_content;
@@ -56,7 +55,7 @@ ajax.onreadystatechange = function(){
 }
 
 //Template dos cards
-var card_item = function(nome, imagem, descricao){
+var card_item = function(nome, imagem, descricao, link){
 
     return '<div class="col-12 col-md-6">'+
                 '<div class="card">'+
@@ -65,9 +64,41 @@ var card_item = function(nome, imagem, descricao){
                 '<h5>'+nome+'</h5>'+
                 '<p class="card-text description">'+descricao+'</p>'+
                 '<div class="col text-center">'+
-                '<button type="button" class="btn btn-link" data-toggle="modal" data-target="'+nome+'">Detalhes</button>'+
+                '<a class="btn btn-link" href="'+link+'" role="button">Currículo Lattes</a>'+
                 '</div>'+
                 '</div>'+
                 '</div>'+
                 '</div>';
+}
+
+//Cache conteúdo dinâmico
+var cache_cards = function(data_json){
+
+    if('caches' in window){
+
+        caches.delete('eeefis-conteudo').then(function(){
+            console.log('Deletando cache de conteúdo antigo');
+
+            if(data_json.length > 0){
+                var files = ['dados.json'];
+
+                //Entrando na categoria
+                for(var i = 0; i < data_json.length; i++){
+
+                    //Entrando no item
+                    for(var j = 0; j < data_json[i].itens.length; j++){
+                        if(files.indexOf(data_json[i].itens[j].imagem) == -1){
+                            files.push(data_json[i].itens[j].imagem);
+                        }
+                    }
+                }
+
+                caches.open('eeefis-conteudo').then(function (cache){
+                    cache.addAll(files).then(function(){
+                        console.log("Arquivos de conteúdo cacheados!");
+                    });
+                });
+            }
+        });
+    }
 }
